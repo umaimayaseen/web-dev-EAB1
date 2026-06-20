acdca467bb6de937bc800657f816272c2f5fe293
umaimayaseen <umaimakh0609@gmail.com>
2026-06-21 02:36:08 +0500

    git hub and todo-list app

---
+++ class-2-todo-list/script.js
@@ -0,0 +1,38 @@
+let todoList = [];
+
+function addingTask() {
+    let inputField = document.getElementById("task-input");
+    let newTask = inputField.value;
+
+    if (newTask === "") {
+        alert("Add Task First!!");
+        return;
+    };
+
+    todoList.push(newTask);
+    inputField.value = "";
+    showList();
+
+}
+
+function showList() {
+    let deliveryBox = document.getElementById("list-container");
+    let finalHTML = "";
+    for (let i = 0; i < todoList.length; i++) {
+        finalHTML = finalHTML + `
+        <p>
+         ${todoList[i]}
+        <button onclick = "deleteTask(${i})" style= "background-color:red;" >Delete</button>
+        </p>
+        `
+
+
+    }
+    deliveryBox.innerHTML = finalHTML;
+}
+
+function deleteTask(index) {
+    todoList.splice(index, 1);
+    showList()
+
+}
